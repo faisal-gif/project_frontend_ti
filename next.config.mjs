019e5457babfbab812bf2149c9c7306a8bca9b3c
faisal-gif <fagdera@gmail.com>
2025-09-01 16:17:03 +0700

    rollback

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        optimizeCss: true, // satukan CSS kecil ke file utama
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
