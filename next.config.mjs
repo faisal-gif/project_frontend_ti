8fd624fc18f9c808762d8f601075feab406f996a
faisal-gif <fagdera@gmail.com>
2025-09-01 16:13:33 +0700

    feat: add experimental CSS optimization in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        optimizeCss: true, // satukan CSS kecil ke file utama
+    },
+};
 
 export default nextConfig;
