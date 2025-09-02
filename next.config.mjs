c5bff6278e663779ea8b72cf268a319dddf1f610
faisal-gif <fagdera@gmail.com>
2025-09-02 14:49:14 +0700

    tambah criters

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn-1.timesmedia.co.id",
-        port: "", // optional
-        pathname: "/**", // allow semua path
-      },
-    ],
-     minimumCacheTTL: 60, // cache di server 60 detik
-  },
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn-1.timesmedia.co.id",
+                port: "", // optional
+                pathname: "/**", // allow semua path
+            },
+        ],
+        minimumCacheTTL: 60, // cache di server 60 detik
+    },
+    experimental: {
+        optimizeCss: true,
+    },
 };
 
 export default nextConfig;
