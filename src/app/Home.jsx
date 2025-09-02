ea27305008b94997b50f1a5f1b4ad530bca9ce42
faisal-gif <fagdera@gmail.com>
2025-09-02 12:50:26 +0700

    new file:   src/app/Home.jsx 	modified:   src/app/layout.jsx 	modified:   src/app/page.jsx

---
+++ src/app/Home.jsx
@@ -0,0 +1,96 @@
+'use client';
+
+import BreakingNews from '@/components/BreakingNews';
+import EKoranSection from '@/components/EKoranSection';
+import FirstHightlightNewsSection from '@/components/FirstHightlightNewsSection';
+import GallerySection from '@/components/GallerySection';
+import GoogleAds from '@/components/GoogleAds';
+import HeadlineNewsHome from '@/components/HeadlineNewsHome';
+import HorizontalNewsSection from '@/components/HorizontalNewsSection';
+import LastestNewsSection from '@/components/LastestNewsSection';
+import PopularNews from '@/components/PopularNews';
+import SimpleNewsSection from '@/components/SimpleNewsSection';
+import VideoSection from '@/components/VideoSection';
+import WeatherCard from '@/components/WeatherCard';
+import { getNewsFirstSections, getNewsSecondSections } from '@/lib/data';
+import { Video } from 'lucide-react';
+import { useEffect, useState } from 'react';
+
+
+
+function Home() {
+
+
+  const [newsFirstSections, setNewsFirstSections] = useState([]);
+  const [newsSecondSections, setNewsSecondSections] = useState([]);
+
+  useEffect(() => {
+    getNewsFirstSections().then(setNewsFirstSections).catch(console.error);
+    getNewsSecondSections().then(setNewsSecondSections).catch(console.error);
+  }, []);
+
+  return (
+    <div className="">
+      <div className='max-w-6xl mx-auto gap-6 pt-18 pb-8 px-4'>
+        <GoogleAds size='banner' />
+      </div>
+      {/* Hero */}
+      <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 pb-8 px-4 md:grid-cols-4">
+        <div className="md:col-span-3">
+          <HeadlineNewsHome />
+        </div>
+        <WeatherCard />
+      </div>
+
+
+      <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 px-4 py-8 md:grid-cols-3">
+        {newsFirstSections.map((section) => (
+          <div key={section.title} className="space-y-8">
+            <SimpleNewsSection title={section.title} news={section.news} />
+          </div>
+        ))}
+      </div>
+
+      <div className="mx-auto max-w-6xl px-4 py-8 ">
+        <EKoranSection />
+      </div>
+
+      <section className="max-w-6xl mx-auto px-6 py-12 max-md:px-4 border-t-2 border-base-300" >
+        <HorizontalNewsSection />
+      </section>
+
+
+      {/* Jurnalistik Fotografi */}
+      <section className="max-w-6xl mx-auto px-6 py-12 max-md:px-4 border-t-2 border-base-300" >
+        <GallerySection />
+      </section>
+
+      <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 px-4 py-8  md:grid-cols-3">
+        {newsSecondSections.map((section) => (
+          <div key={section.title} className="space-y-8">
+            <FirstHightlightNewsSection title={section.title} news={section.news} />
+          </div>
+        ))}
+      </div>
+
+      <VideoSection />
+
+      <div className="max-w-6xl mx-auto px-6 py-8 max-md:px-4">
+        <div className='grid grid-cols-1  gap-4 md:grid-cols-6 md:gap-4'>
+          <div className="md:col-span-4 lg:col-span-4">
+            <LastestNewsSection />
+          </div>
+
+          <div className="md:col-span-2 lg:col-span-2 ">
+            <PopularNews />
+            <GoogleAds size='rectangle' />
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default Home;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,93 +1,16 @@
-'use client';
+import React from 'react'
+import Home from './Home'
 
-import BreakingNews from '@/components/BreakingNews';
-import EKoranSection from '@/components/EKoranSection';
-import FirstHightlightNewsSection from '@/components/FirstHightlightNewsSection';
-import GallerySection from '@/components/GallerySection';
-import GoogleAds from '@/components/GoogleAds';
-import HeadlineNewsHome from '@/components/HeadlineNewsHome';
-import HorizontalNewsSection from '@/components/HorizontalNewsSection';
-import LastestNewsSection from '@/components/LastestNewsSection';
-import PopularNews from '@/components/PopularNews';
-import SimpleNewsSection from '@/components/SimpleNewsSection';
-import VideoSection from '@/components/VideoSection';
-import WeatherCard from '@/components/WeatherCard';
-import { getNewsFirstSections, getNewsSecondSections } from '@/lib/data';
-import { Video } from 'lucide-react';
-import { useEffect, useState } from 'react';
+export const metadata = {
+  title: "TIMES Indonesia - Berita Positif Terbaru dan Terkini",
+  description: "Portal berita positif yang menyajikan informasi terkini tentang peristiwa, cek fakta, ekoran, politik, entertainment, kuliner, gaya hidup, wisata, dan kopi times",
+  keywords: "times indonesia, timesindonesia, portal berita, berita positif, berita terbaru, berita terkini, informasi terkini, informasi terbaru, peristiwa, cek fakta, ekoran, politik, entertainment, kuliner, gaya hidup, wisata, dan kopi times",
+};
 
-function Home() {
-
-  const [newsFirstSections, setNewsFirstSections] = useState([]);
-  const [newsSecondSections, setNewsSecondSections] = useState([]);
-
-  useEffect(() => {
-    getNewsFirstSections().then(setNewsFirstSections).catch(console.error);
-    getNewsSecondSections().then(setNewsSecondSections).catch(console.error);
-  }, []);
-
+function page() {
   return (
-    <div className="">
-      <div className='max-w-6xl mx-auto gap-6 pt-18 pb-8 px-4'>
-        <GoogleAds size='banner' />
-      </div>
-      {/* Hero */}
-      <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 pb-8 px-4 md:grid-cols-4">
-        <div className="md:col-span-3">
-          <HeadlineNewsHome />
-        </div>
-        <WeatherCard />
-      </div>
-
-
-      <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 px-4 py-8 md:grid-cols-3">
-        {newsFirstSections.map((section) => (
-          <div key={section.title} className="space-y-8">
-            <SimpleNewsSection title={section.title} news={section.news} />
-          </div>
-        ))}
-      </div>
-
-      <div className="mx-auto max-w-6xl px-4 py-8 ">
-        <EKoranSection />
-      </div>
-
-      <section className="max-w-6xl mx-auto px-6 py-12 max-md:px-4 border-t-2 border-base-300" >
-        <HorizontalNewsSection />
-      </section>
-
-
-      {/* Jurnalistik Fotografi */}
-      <section className="max-w-6xl mx-auto px-6 py-12 max-md:px-4 border-t-2 border-base-300" >
-        <GallerySection />
-      </section>
-      
-      <div className="mx-auto grid max-w-6xl grid-cols-1 gap-6 px-4 py-8  md:grid-cols-3">
-        {newsSecondSections.map((section) => (
-          <div key={section.title} className="space-y-8">
-            <FirstHightlightNewsSection title={section.title} news={section.news} />
-          </div>
-        ))}
-      </div>
-
-      <VideoSection />
-
-      <div className="max-w-6xl mx-auto px-6 py-8 max-md:px-4">
-        <div className='grid grid-cols-1  gap-4 md:grid-cols-6 md:gap-4'>
-          <div className="md:col-span-4 lg:col-span-4">
-            <LastestNewsSection />
-          </div>
-
-          <div className="md:col-span-2 lg:col-span-2 ">
-            <PopularNews />
-            <GoogleAds size='rectangle' />
-          </div>
-        </div>
-      </div>
-
-
-    </div>
-  );
+    <Home />
+  )
 }
 
-export default Home;
+export default page
