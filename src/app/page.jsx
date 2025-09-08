c32ea8ed8a349f5cd374d05a5f7da0cc1bcf7769
faisal-gif <fagdera@gmail.com>
2025-09-08 13:42:59 +0700

    modified:   src/app/page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,23 @@ export const metadata = {
   title: "TIMES Indonesia - Berita Positif Terbaru dan Terkini",
   description: "Portal berita positif yang menyajikan informasi terkini tentang peristiwa, cek fakta, ekoran, politik, entertainment, kuliner, gaya hidup, wisata, dan kopi times",
   keywords: "times indonesia, timesindonesia, portal berita, berita positif, berita terbaru, berita terkini, informasi terkini, informasi terbaru, peristiwa, cek fakta, ekoran, politik, entertainment, kuliner, gaya hidup, wisata, dan kopi times",
-
+  openGraph: {
+    locale: 'id_ID',
+    title: "TIMES Indonesia - Berita Positif Terbaru dan Terkini",
+    description: "Portal berita positif yang menyajikan informasi terkini tentang peristiwa, cek fakta, ekoran, politik, entertainment, kuliner, gaya hidup, wisata, dan kopi times",
+    keywords: "times indonesia, timesindonesia, portal berita, berita positif, berita terbaru, berita terkini, informasi terkini, informasi terbaru, peristiwa, cek fakta, ekoran, politik, entertainment, kuliner, gaya hidup, wisata, dan kopi times",
+    images: [
+      {
+        url: '/logo.png',
+        width: 1200,
+        height: 630,
+        alt: 'TIMES Indonesia Logo',
+      },
+    ],
+    type: "web",
+    url: 'https://timesindonesia.co.id',
+    siteName: 'TIMES Indonesia',
+  }
 };
 
 function page() {
