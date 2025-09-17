0059d4d17c08826bf0aa7c1cf9857fed8e78f538
faisal-gif <fagdera@gmail.com>
2025-09-17 14:43:53 +0700

    ekoran skeleton update

--- src/components/ui/EkoranHomeCardSkeleton.jsx
+++ src/components/ui/EkoranHomeCardSkeleton.jsx
@@ -5,8 +5,7 @@ function EkoranHomeCardSkeleton() {
         {/* Cover Skeleton */}
         <div className="aspect-[6/8] relative overflow-hidden rounded-t-lg  bg-gray-300 dark:bg-gray-700" />
 
-        {/* View Counter Skeleton */}
-        <div className="absolute bottom-3 right-3 bg-base-300 rounded-md w-16 h-5" />
+     
       </div>
 
     
