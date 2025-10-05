dca4c2c9ee9dfb202fc853f077fe65fd6c13da6b
faisal-gif <fagdera@gmail.com>
2025-10-05 18:28:03 +0700

    modified:   src/app/sitemap/kanal/news/[slug]/sitemap.xml/route.js 	modified:   src/app/sitemap/kanal/web/[slug]/sitemap.xml/route.js

--- src/app/sitemap/kanal/web/[slug]/sitemap.xml/route.js
+++ src/app/sitemap/kanal/web/[slug]/sitemap.xml/route.js
@@ -24,7 +24,7 @@ export async function GET(request, { params }) {
                 (item) => `
     <url>
         <loc>${process.env.NEXT_PUBLIC_URL}${item.url_ci4}</loc>
-          <lastmod>${new Date().toISOString(item.news_datepub)}</lastmod>
+          <lastmod>${new Date(item.news_datepub).toISOString()}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.8</priority>
     </url>`
