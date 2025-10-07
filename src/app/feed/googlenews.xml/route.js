d2f832adade4263e883a25fd977ed64d1f9d5bac
faisal-gif <fagdera@gmail.com>
2025-10-07 22:50:41 +0700

    modified:   src/app/feed/googlenews.xml/route.js

--- src/app/feed/googlenews.xml/route.js
+++ src/app/feed/googlenews.xml/route.js
@@ -20,7 +20,6 @@ export async function GET() {
         console.error("Error fetch focus:", error);
         news = []; // fallback supaya tidak crash
     }
-    const lastModDate = new Date().toISOString();
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
@@ -30,7 +29,7 @@ export async function GET() {
                 (item) => `
     <url>
         <loc>${process.env.NEXT_PUBLIC_URL}${item.url_ci4}</loc>
-        <lastmod>${lastModDate}</lastmod>
+        <lastmod>${new Date(item.news_datepub).toISOString()}</lastmod>
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
       <news:news>
