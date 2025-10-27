46c9af75513c539f70117a5ac8b8cc50495c2552
faisal-gif <fagdera@gmail.com>
2025-10-27 11:32:55 +0700

    modified:   src/app/ekoran/[id]/[slug]/page.jsx

--- src/app/ekoran/[id]/[slug]/page.jsx
+++ src/app/ekoran/[id]/[slug]/page.jsx
@@ -33,8 +33,8 @@ export async function generateMetadata({ params }) {
       images: [
         {
           url: mediumImageUrl,
-          width: 1200,
-          height: 630,
+          width: 500,
+          height: 750,
           alt: ekoranDetail.title,
         },
       ],
@@ -47,8 +47,8 @@ export async function generateMetadata({ params }) {
       images: [
         {
           url: ekoranDetail.img1,
-          width: 1200,
-          height: 630,
+          width: 500,
+          height: 750,
           alt: ekoranDetail.title,
         },
       ],
