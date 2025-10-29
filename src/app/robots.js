a17d91edfc9b1553f98070e4c631aa054a5fa4a8
faisal-gif <fagdera@gmail.com>
2025-10-29 08:14:40 +0700

    update robots

--- src/app/robots.js
+++ src/app/robots.js
@@ -7,6 +7,16 @@ export default function robots() {
         allow: '/',
         disallow: ['/search/*', '/search*']
       },
+      {
+        userAgent: 'SemrushBot',
+        crawlDelay: 10,
+        allow: '/',
+      },
+      {
+        userAgent: 'SerpstatBot',
+        crawlDelay: 10,
+        allow: '/',
+      },
 
     ],
     sitemap: 'https://www.timesindonesia.co.id/sitemap.xml',
