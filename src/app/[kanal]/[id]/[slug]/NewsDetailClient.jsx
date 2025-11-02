d411959438dfa1e7db3169c698b60a94cd12499b
faisal-gif <fagdera@gmail.com>
2025-11-02 17:38:26 +0700

    modified:   src/app/[kanal]/[id]/[slug]/NewsDetailClient.jsx

--- src/app/[kanal]/[id]/[slug]/NewsDetailClient.jsx
+++ src/app/[kanal]/[id]/[slug]/NewsDetailClient.jsx
@@ -403,7 +403,7 @@ function NewsDetailClient({ initialNewsDetail, initialWriter, initAllNews }) {
                 </aside>
             </div>
 
-            <div>
+            {/* <div>
                 <div className="flex mt-8 items-center justify-between">
                     <h2 className="flex gap-2 items-center text-2xl font-bold text-foreground">
                         <div className="w-1 h-6 bg-[#C31815] rounded-full"></div>
@@ -427,7 +427,7 @@ function NewsDetailClient({ initialNewsDetail, initialWriter, initAllNews }) {
                     ))}
 
                 </div>
-            </div>
+            </div> */}
             <ModalShare title={newsDetail.news_title} url={`${process.env.NEXT_PUBLIC_URL}${newsDetail.url_ci4}`} />
         </div>
 
