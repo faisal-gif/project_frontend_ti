15a29c5c62d788a9399070f35445ced3cb8e6ee7
faisal-gif <fagdera@gmail.com>
2025-11-11 14:17:47 +0700

    new file:   src/components/ATIWidget.jsx

---
+++ src/components/ATIWidget.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ATIWidget() {
+    return (
+        <iframe
+            src="https://ati.timesindonesia.co.id/widget"
+            title="Widget ATI Times.co.id"
+            frameBorder="0"
+            loading="lazy"
+            className="w-full border-none overflow-hidden h-[700px] md:h-[800px]"
+        />
+    )
+}
+
+export default ATIWidget
