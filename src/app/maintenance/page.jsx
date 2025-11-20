51bea84f83aeef8e25ad2a046ad513c8c899a43e
faisal-gif <fagdera@gmail.com>
2025-11-20 22:18:16 +0700

    modified:   src/app/maintenance/page.jsx

--- src/app/maintenance/page.jsx
+++ src/app/maintenance/page.jsx
@@ -1,8 +1,15 @@
 export default function Maintenance() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-center p-10">
-      <h1 className="text-4xl font-bold mb-4">Maintenance Mode</h1>
-      <p className="text-lg">Website sedang dalam perbaikan. Silakan kembali nanti.</p>
-    </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center text-center p-10">
+            <h1 className="text-4xl font-bold mb-4">Maintenance Mode</h1>
+            <p className="text-lg">Website sedang dalam perbaikan. Silakan kembali nanti.</p>
+
+            <a
+                href="/"
+                className="btn btn-primary rounded-2xl mt-8"
+            >
+                Kembali ke Beranda
+            </a>
+        </div>
+    );
 }
