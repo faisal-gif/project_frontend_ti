093f6211a5f5cbf15300b3d9b9f95e9f0bcb8b8d
faisal-gif <fagdera@gmail.com>
2025-11-29 13:10:26 +0700

    modified:   src/app/tag/[slug]/page.jsx

--- src/app/tag/[slug]/page.jsx
+++ src/app/tag/[slug]/page.jsx
@@ -1,8 +1,8 @@
 // app/.../page.jsx
 
-// import React from 'react'
-// import TagClient from './TagClient'
-// import { getAllNewsServer } from '@/lib/api/newsApi';
+import React from 'react'
+import TagClient from './TagClient'
+import { getAllNewsServer } from '@/lib/api/newsApi';
 import { redirect } from 'next/navigation';
 
 
@@ -15,80 +15,80 @@ const unslugify = (slug) => {
     .replace(/\b\w/g, (c) => c.toUpperCase());
 };
 
-// export async function generateMetadata({ params }) {
-//   const { slug } = await params;
-//   const tagTitle = unslugify(slug);
+export async function generateMetadata({ params }) {
+  const { slug } = await params;
+  const tagTitle = unslugify(slug);
 
-//   if (!slug) {
-//     return {
-//       title: "Berita tidak ditemukan - TIMES Indonesia",
-//       description: "Berita yang Anda cari tidak tersedia.",
-//     };
-//   }
+  if (!slug) {
+    return {
+      title: "Berita tidak ditemukan - TIMES Indonesia",
+      description: "Berita yang Anda cari tidak tersedia.",
+    };
+  }
 
-//   return {
-//     title: `${tagTitle} - TIMES Indonesia`,
-//     openGraph: {
-//       locale: 'id_ID',
-//       title: tagTitle,
-//       description: tagTitle,
-//       images: [
-//         {
-//           url: `${process.env.NEXT_PUBLIC_URL}/icon.png`,
-//           width: 500,
-//           height: 500,
-//           alt: "TIMES Indonesia Logo",
-//         },
-//       ],
-//       type: "website",
-//       url: `https://timesindonesia.co.id/tag/${tagTitle}`,
-//       siteName: 'TIMES Indonesia',
-//     },
-//     twitter: {
-//       card: "summary_large_image",
-//       title: tagTitle,
-//       description: tagTitle,
-//       images: [
-//         {
-//           url: `${process.env.NEXT_PUBLIC_URL}/icon.png`,
-//           width: 500,
-//           height: 500,
-//           alt: "TIMES Indonesia Logo",
-//         },
-//       ],
-//     },
-//   };
-// }
+  return {
+    title: `${tagTitle} - TIMES Indonesia`,
+    openGraph: {
+      locale: 'id_ID',
+      title: tagTitle,
+      description: tagTitle,
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_URL}/icon.png`,
+          width: 500,
+          height: 500,
+          alt: "TIMES Indonesia Logo",
+        },
+      ],
+      type: "website",
+      url: `https://timesindonesia.co.id/tag/${tagTitle}`,
+      siteName: 'TIMES Indonesia',
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: tagTitle,
+      description: tagTitle,
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_URL}/icon.png`,
+          width: 500,
+          height: 500,
+          alt: "TIMES Indonesia Logo",
+        },
+      ],
+    },
+  };
+}
 
 
 async function page({ params }) {
 
-  redirect("/maintenance");
+  // redirect("/maintenance");
 
-  // const { slug } = await params;
-  // const tag = unslugify(slug);
-  // const limit = 9;
-  // let initialNews = [];
+  const { slug } = await params;
+  const tag = unslugify(slug);
+  const limit = 9;
+  let initialNews = [];
 
-  // try {
-  //   initialNews = await getAllNewsServer({
-  //     news_type: 'tag',
-  //     title: tag,
-  //     limit: limit,
-  //     offset: 0,
-  //   });
-  // } catch (error) {
-  //   console.error("Failed to fetch initial news:", error);
-  // }
+  try {
+    initialNews = await getAllNewsServer({
+      news_type: 'tag',
+      title: tag,
+      limit: limit,
+      offset: 0,
+    });
+  } catch (error) {
+    console.error("Failed to fetch initial news:", error);
+  }
 
-  // return (
+  return (
 
-  //   <TagClient
-  //     initialNews={initialNews || []}
-  //     slug={slug}
-  //     unslugifiedSlug={tag}
-  //   />
-  // )
+    <TagClient
+      initialNews={initialNews || []}
+      slug={slug}
+      unslugifiedSlug={tag}
+    />
+  )
 }
 
 export default page
