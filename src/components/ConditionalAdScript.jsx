554279d6e31290384a810e88f362f1d06e0058b6
faisal-gif <fagdera@gmail.com>
2025-12-17 10:56:26 +0700

    Update DISABLED_AD_PATHS in ConditionalAdScript to include additional URLs

--- src/components/ConditionalAdScript.jsx
+++ src/components/ConditionalAdScript.jsx
@@ -11,7 +11,21 @@ const DISABLED_AD_PATHS = [
     '/glutera-news/432953/5-cara-ampuh-membuat-wanita-orgasme-berkalikali',
     '/glutera-news/394036/7-tanda-wanita-mencapai-kenikmatan',
     '/cek-fakta/500746/cek-fakta-hoaks-video-mayat-tergantung-di-jembatan-layang-kotalama-malang',
-    '/glutera-news/374548/4-tips-nyaman-saat-hubungan-suami-istri-tanpa-rasa-nyeri'
+    '/glutera-news/374548/4-tips-nyaman-saat-hubungan-suami-istri-tanpa-rasa-nyeri',
+    '/glutera-news/380365/6-gerakan-fitnes-yang-bikin-pria-makin-perkasa-di-ranjang',
+    '/glutera-news/145748/heboh-glutera-sexy-award-2017-54-wanita-cantik-adu-bodi',
+    '/peristiwa/104960/waduh--praktik-prostitusi-ternyata-masih-ada-di-malang',
+    '/glutera-news/210144/buah-jeruk-dan-wanita-setengah-baya',
+    '/glutera-news/226244/tips-tidak-cepat-kehabisan-napas-saat-berhubungan-intim',
+    '/glutera-news/365758/7-faktor-yang-bikin-orgasme-hebat',
+    '/gaya-hidup/246607/haji-iin-sang-kolektor-pipa-cangklong-dari-Yogyakarta',
+    '/pendidikan/377041/inspiratif-pemuda-kampung-di-pangandaran-berhasil-dirikan-taman-bacaan-masyarakat',
+    '/glutera-news/374373/16-katakata-nakal-yang-bisa-diucapkan-saat-bercinta',
+    '/glutera-news/373635/6-hal-yang-disukai-pria-dari-wanita-saat-berhubungan-seksual',
+    '/gaya-hidup/398043/doa-serta-adab-berhubungan-suami-istri-dalam-kitab-qurrotul-uyun-dan-fathul-izar',
+    '/peristiwa/117515/jiad-kalau-bersih-mui-tak-usah-takut-diaudit',
+    '/politik/486020/gawagus-muda-jombang-haramkan-pilih-pemimpin-cacat-etik-dan-moral',
+    '/glutera-news/373804/8-bagian-tubuh-pria-yang-mudah-terangsang'
 ];
 
 export default function ConditionalAdScript() {
