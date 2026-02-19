bc13b2b90a5fde1e32acf3acd6b4294e612a3557
faisal-gif <fagdera@gmail.com>
2026-02-19 13:37:16 +0700

    modified:   src/app/feed/all/[slug]/route.js

--- src/app/feed/all/[slug]/route.js
+++ src/app/feed/all/[slug]/route.js
@@ -86,8 +86,8 @@ export async function GET(request, { params }) {
 
 
     const rssFeed = `<?xml version="1.0" encoding="UTF-8"?>
-<rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
-  <channel>
+<rss xmlns:content="http://purl.org/rss/1.0/modules/content/" version="2.0">
+    <channel>
     <title><![CDATA[RSS Feed TIMES Indonesia - ${kanal.catnews_title}]]></title>
     <description><![CDATA[Berita Positif Terbaru dan Terkini]]></description>
     <link>${process.env.NEXT_PUBLIC_URL}</link>
