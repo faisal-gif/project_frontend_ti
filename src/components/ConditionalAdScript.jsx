c4e4bce709654db6b5380d2f868af37321dca90f
faisal-gif <fagdera@gmail.com>
2026-03-28 12:31:30 +0700

    modified:   src/components/ConditionalAdScript.jsx

--- src/components/ConditionalAdScript.jsx
+++ src/components/ConditionalAdScript.jsx
@@ -46,7 +46,7 @@ export default function ConditionalAdScript() {
     return (
         <Script
             id="adsbygoogle-init"
-            strategy="afterInteractive"
+            strategy="lazyOnload"
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2259519132704244"
             crossOrigin="anonymous"
