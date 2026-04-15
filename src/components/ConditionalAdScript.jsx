c1217c7b674a55440e7670b0b6871acdaf81c63c
faisal-gif <fagdera@gmail.com>
2026-04-15 16:31:56 +0700

    fix: change ad loading strategy to lazyOnload in ConditionalAdScript component

--- src/components/ConditionalAdScript.jsx
+++ src/components/ConditionalAdScript.jsx
@@ -80,7 +80,7 @@ export default function ConditionalAdScript() {
     return (
         <Script
             id="adsbygoogle-init"
-            strategy="worker"
+            strategy="lazyOnload"
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2259519132704244"
             crossOrigin="anonymous"
